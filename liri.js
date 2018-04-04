5a21cadfed42b5e8b30e944cd2786a48d3f5aad5
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-04-04 18:11:47 -0400

    another

--- liri.js
+++ liri.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
 
 console.log(process.env.SPOTIFY_ID=your-spotify-id);
 console.log(process.env.SPOTIFY_SECRET=your-spotify-secret);
