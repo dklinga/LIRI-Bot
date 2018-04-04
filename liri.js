d03b52d3e8746299dda416a75404be65cf3fd7f5
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-04-04 08:48:39 -0400

    first

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+
+console.log(process.env.SPOTIFY_ID=your-spotify-id);
+console.log(process.env.SPOTIFY_SECRET=your-spotify-secret);
+console.log(process.env.SPOTIFY_ID=your-spotify-id);
+console.log(process.env.SPOTIFY_ID=your-spotify-id);
