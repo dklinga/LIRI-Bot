f4d83109d8def05f3ace8d88e4af3d0c41653404
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-04-04 20:00:45 -0400

    new

--- liri.js
+++ liri.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
